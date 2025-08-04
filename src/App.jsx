9465e9963cd2fc866c6066c29b168ee4fe17fd91
Alan <herculinoalan@gmail.com>
2025-08-04 10:29:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return <>
+  <div className="min-h-screen bg-blue-900 flex justify-center items-center px-4">
+    <div className='flex justify-between items-center space-x-2'>
+        question
+    </div>
+  </div>
+  </>;
+}
+
+export default App;
